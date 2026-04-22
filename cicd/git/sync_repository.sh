952086bc5ace7c84b2044b0876101f8e7286c229
hoontaky <hoontaky@169.254.6.88>
2026-04-22 09:47:54 +0900

    Add Info.

---
+++ cicd/git/sync_repository.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git clone -b <original_git_branch> https://<original_git_url>/<git_repo>.git
+
+cd ./<original_git_repo>
+
+git remote -v
+git remote set-url origin http://<target_git_url>/<git_repo>.git
+git remote -v
+git push -uf origin <target_git_branch>
+
+exit 1
+
+----- for example -----
+
+git clone -b develop http://hoontaky@127.0.0.1:8090/com/common.git
+
+cd ./common
+
+git remote -v
+git remote set-url origin http://root@127.0.0.1:9090/com/common.git
+git remote -v
+git push -uf origin develop
+
+exit 1
