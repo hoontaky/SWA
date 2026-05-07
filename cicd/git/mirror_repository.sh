d26c638564b4f5265b20a70778897a5851713fd7
hoontaky <hoontaky@169.254.6.88>
2026-05-07 16:02:14 +0900

    Update Info.

--- cicd/git/mirror_repository.sh
+++ cicd/git/mirror_repository.sh
@@ -24,18 +24,28 @@ exit 1
 
 withCredentials([usernamePassword(credentialsId: 'jenkins', passwordVariable: 'password', usernameVariable: 'userName')]) {
 	sh """
-		git clone --mirror http://hoontaky@127.0.0.1:8090/com/common.git
+		git clone --mirror http://${userName}:${password}@127.0.0.1:8090/com/common.git
 	"""
 }
 
 withCredentials([usernamePassword(credentialsId: 'jenkins', passwordVariable: 'password', usernameVariable: 'userName')]) {
 	
-	// for 사용 시에는 "target" 사용하면 오류 발생함. git은 저장소 명을 하나만 사용 가능함. >> for 문 key 값 사용 권장
-	
 	dir('./common.git') {
 		sh """
-			git remote add target http://root@127.0.0.1:9090/com/common.git
+			git remote add target http://${userName}:${password}@127.0.0.1:9090/com/common.git
 			git push target --mirror
 		"""
+		
+		<!--
+			// for 사용 시에는 "target" 사용하면 오류 발생함. git은 저장소 명을 하나만 사용 가능함. >> for 문 key 값 사용 권장
+			ex)
+			for (int inx=0; inx<keyList.size(); inx++) {
+				def key = keyList[inx]
+				sh """
+					git remote add ${key} http://${userName}:${password}@127.0.0.1:9090/com/common.git
+					git push ${key} --mirror
+				"""
+			}
+		-->
 	}
 }
