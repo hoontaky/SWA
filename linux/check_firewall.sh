b31efc93fdc5d565be49d16504712fedbb788231
hoontaky <hoontaky@P00078379-004.CNS.LGCNS.COM>
2026-07-21 15:33:04 +0900

    Update Info.

--- linux/check_firewall.sh
+++ linux/check_firewall.sh
@@ -2,6 +2,7 @@
 
 #IP List
 ips=("192.168.0.1" "192.168.0.2" "192.168.0.3" "192.168.0.4")
+port=22
 
 i=0
 len=${#ips[@]}
@@ -11,7 +12,7 @@ while [ $i -lt $len ]; do
 	
 	echo "check ip :: " $ip
 	
-	nc -w 1 -i 1 -v $ip 22
+	nc -v -w 1 -i 1 $ip $port
 	
 	((i++))
 done
